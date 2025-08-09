a1e3141c335527af9c0e0955cf914c649817e236
athidh <athidhvasuj123@gmail.com>
2025-08-09 22:34:09 +0530

    testing

--- veydha-backend/app.js
+++ veydha-backend/app.js
@@ -8,27 +8,7 @@ const authRoutes = require('./routes/authRoutes');
 const patientRoutes = require('./routes/patientRoutes');
 
 const app = express();
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:5173',
-  'http://localhost:8080',
-];
-
-// This new line handles the browser's pre-flight request for CORS
-app.options('*', cors());
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from your origin.';
-      return callback(new Error(msg), false);
-    }
-
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.use(express.json());
 
