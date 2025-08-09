7595e3f62ea3f924a846b41fe650fdf6a7d0b6c5
athidh <athidhvasuj123@gmail.com>
2025-08-09 14:30:58 +0530

    app.js

--- veydha-backend/app.js
+++ veydha-backend/app.js
@@ -8,8 +8,23 @@ const authRoutes = require('./routes/authRoutes');
 const patientRoutes = require('./routes/patientRoutes');
 
 const app = express();
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173', 
+];
 
-app.use(cors());
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from your origin.';
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  }
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
