1f4cd9799b8e1dfd484695b564b9dc5fdcbf87b6
athidh <athidhvasuj123@gmail.com>
2025-08-09 23:58:05 +0530

    fetching complete data

--- veydha-backend/controllers/patientController.js
+++ veydha-backend/controllers/patientController.js
@@ -1,14 +1,23 @@
 const Patient = require('../models/patientModel');
-
 const getMyProfile = async (req, res) => {
     try {
+        
         const patient = await Patient.findById(req.user.id);
 
         if (!patient) {
             return res.status(404).json({ message: 'Patient not found' });
         }
-        console.log('Patient data on server:', patient);
-        res.status(200).json(patient);
+        const patientProfile = {
+            _id: patient._id,
+            patientId: patient.patientId,
+            name: patient.name,
+            age: patient.age,
+            gender: patient.gender,
+            consultationHistory: patient.consultationHistory, 
+            createdAt: patient.createdAt,
+            updatedAt: patient.updatedAt
+        };
+        res.status(200).json(patientProfile);
 
     } catch (error) {
         res.status(500).json({ message: `Server Error: ${error.message}` });
